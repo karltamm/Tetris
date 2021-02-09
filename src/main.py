7fb453c50304506e49859b5c604348381b282466
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-02-09 22:26:19 +0200

    Code is formatted

--- src/main.py
+++ src/main.py
@@ -1,39 +1,40 @@
 import pygame
 from board import *
+from smallboard import *
 from blocks import *
 from screen import *
+
 
 # MAIN
 def main():
     pygame.init()
     CLOCK = pygame.time.Clock()
 
-    board = createBoard() # 2D array, where "0" represents empty cell
+    board = createBoard()  # 2D array, where "0" represents empty cell
     small_board = createSmallBoard()
 
     current_block = activeBlock(0, board)
-    next_block = randomBlock(small_board,0,1)
-    third_block = randomBlock(small_board,0,6)
+    next_block = randomBlock(small_board, 0, 1)
+    third_block = randomBlock(small_board, 0, 6)
 
-    
     # For holding down keys
     down_pressed = False
     left_pressed = False
     right_pressed = False
     key_timer = 0
-    
+
     # Block automatic falling
     fall_timer = 0
-    FALL_SPEED = 25 # Lower value -> Faster drop speed
+    FALL_SPEED = 25  # Lower value -> Faster drop speed
     updateSmallBoard(small_board)
 
     run = True
     while run:
         CLOCK.tick(FPS)
-        
+
         # For holding down keys
         key_timer += 1
-        
+
         # Block automatic falling
         fall_timer += 1
 
@@ -47,9 +48,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-            
+
             # Move block
-            if event.type == pygame.KEYDOWN: # If a key is pressed down
+            if event.type == pygame.KEYDOWN:  # If a key is pressed down
                 key_timer = 0
                 if event.key == pygame.K_UP:
                     current_block.rotate(board)
@@ -59,21 +60,21 @@ def main():
                     right_pressed = True
                 elif event.key == pygame.K_LEFT:
                     left_pressed = True
-                    
-            elif event.type == pygame.KEYUP: # If a key is released
+
+            elif event.type == pygame.KEYUP:  # If a key is released
                 if event.key == pygame.K_DOWN:
                     down_pressed = False
                 elif event.key == pygame.K_RIGHT:
                     right_pressed = False
                 elif event.key == pygame.K_LEFT:
                     left_pressed = False
-                    
+
         if down_pressed:
-                fall_timer += 8
+            fall_timer += 8
         if right_pressed and key_timer % 10 == 0:
-                current_block.move(board, 1, 0)
+            current_block.move(board, 1, 0)
         if left_pressed and key_timer % 10 == 0:
-                current_block.move(board, -1, 0)
+            current_block.move(board, -1, 0)
 
         # Is current block placed?
         if current_block.is_placed == True:
@@ -87,5 +88,6 @@ def main():
 
         # Screen
         updateMainBoard(board)
+
 
 main()
--- src/smallboard.py
+++ src/smallboard.py
@@ -2,6 +2,7 @@ import random
 from shapes import *
 from board import *
 
+# CONSTANTS
 SMALLBOARD_WIDTH = 4  # Number of board cells in a row
 SMALLBOARD_HEIGHT = 11  # Number of board cells in a column
 
@@ -24,9 +25,11 @@ class NextBlocks:
                 copySmallBoard(new_board, small_board)
                 new_board[row][col] = block_cell
 
+
 # FUNCTIONS
 def createSmallBoard():
-    return [[0]*SMALLBOARD_WIDTH for row in range(SMALLBOARD_HEIGHT)] # 2D array, where "0" represents empty cell
+    return [[0] * SMALLBOARD_WIDTH for row in range(SMALLBOARD_HEIGHT)]  # 2D array, where "0" represents empty cell
+
 
 def copySmallBoard(src, dest=0):
     if dest == 0:
@@ -35,9 +38,11 @@ def copySmallBoard(src, dest=0):
     for row in range(SMALLBOARD_HEIGHT):
         for col in range(SMALLBOARD_WIDTH):
             dest[row][col] = src[row][col]
+
     return dest
 
-def randomBlock(small_board,x,y):
+
+def randomBlock(small_board, x, y):
     block = random.choice(SHAPES)
-    NextBlocks(block,small_board,x,y)
+    NextBlocks(block, small_board, x, y)
     return block
