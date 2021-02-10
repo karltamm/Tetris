3ab3adb36ba987250b1e0c0b99dce6ef1b14e1de
adaresa <kasper.vaus0@gmail.com>
2021-02-10 02:06:05 +0200

    assets 2.0
    cell .png files replaced with easily editable rectangles

--- src/assets.py
+++ src/assets.py
@@ -1,12 +1,68 @@
 import pygame
 import os
+pygame.init()
 
 # Cells
-EMPTY_CELL = pygame.image.load(os.path.join("assets/cells", "empty.png"))
-GREEN_CELL = pygame.image.load(os.path.join("assets/cells", "green.png"))
-INDIGO_CELL = pygame.image.load(os.path.join("assets/cells", "indigo.png"))
-ORANGE_CELL = pygame.image.load(os.path.join("assets/cells", "orange.png"))
-PINK_CELL = pygame.image.load(os.path.join("assets/cells", "pink.png"))
-RED_CELL = pygame.image.load(os.path.join("assets/cells", "red.png"))
-YELLOW_CELL = pygame.image.load(os.path.join("assets/cells", "yellow.png"))
-BLUE_CELL = pygame.image.load(os.path.join("assets/cells", "blue.png"))
+CELL_SIZE = (20, 20)
+# COLORS
+CELL_OUTLINE = (100, 100, 100) # Grey
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# White outline for big box
+BIG_BACKGROUND = pygame.Surface((200, 400), pygame.SRCALPHA) 
+pygame.draw.rect(BIG_BACKGROUND,(WHITE), (0, 0, 200, 400), 3)
+
+# White outline for small box
+SMALL_BACKGROUND = pygame.Surface((80, 220), pygame.SRCALPHA)
+pygame.draw.rect(SMALL_BACKGROUND,(WHITE), (0, 0, 80, 220), 3)
+
+# EMPTY CELL
+EMPTY_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(EMPTY_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# GREEN_CELL
+GREEN_CELL = pygame.Surface(CELL_SIZE) # GIVE SURFACE SIZE OF CELL_SIZE
+pygame.draw.rect(GREEN_CELL, (0, 255, 0), (0, 0, 20, 20)) # FILL SURFACE WITH RGB VALUE OF (0, 255, 0)
+pygame.draw.rect(GREEN_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1) # DRAW 1 PIXEL OUTLINE OF COLOR "CELL_OUTLINE"
+
+# INDIGO_CELL
+INDIGO_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(INDIGO_CELL, (136, 77, 255), (0, 0, 20, 20))
+pygame.draw.rect(INDIGO_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# ORANGE_CELL
+ORANGE_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(ORANGE_CELL, (255, 100, 0), (0, 0, 20, 20))
+pygame.draw.rect(ORANGE_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# PINK_CELL
+PINK_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(PINK_CELL, (255, 128, 234), (0, 0, 20, 20))
+pygame.draw.rect(PINK_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# YELLOW_CELL
+YELLOW_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(YELLOW_CELL, (255, 255, 0), (0, 0, 20, 20))
+pygame.draw.rect(YELLOW_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# RED_CELL
+RED_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(RED_CELL, (255, 0, 0), (0, 0, 20, 20))
+pygame.draw.rect(RED_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# BLUE_CELL
+BLUE_CELL = pygame.Surface(CELL_SIZE)
+pygame.draw.rect(BLUE_CELL, (0, 0, 255), (0, 0, 20, 20))
+pygame.draw.rect(BLUE_CELL, CELL_OUTLINE, (0, 0, 20, 20), 1)
+
+# Fonts
+font1 = pygame.font.SysFont(None, 30)
+font2 = pygame.font.SysFont('arial', 45)
+
+# Functions
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
