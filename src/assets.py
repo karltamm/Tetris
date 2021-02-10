65a5127447d07e32c633ad04471cfb1a9a1e923c
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-02-10 13:09:36 +0200

    UI improvement

--- src/assets.py
+++ src/assets.py
@@ -1,12 +1,26 @@
 import pygame
+import pygame.freetype
 import os
+
+pygame.init()
 
 # Cells
 EMPTY_CELL = pygame.image.load(os.path.join("assets/cells", "empty.png"))
 GREEN_CELL = pygame.image.load(os.path.join("assets/cells", "green.png"))
-INDIGO_CELL = pygame.image.load(os.path.join("assets/cells", "indigo.png"))
-ORANGE_CELL = pygame.image.load(os.path.join("assets/cells", "orange.png"))
+BRONZE_CELL = pygame.image.load(os.path.join("assets/cells", "bronze.png"))
+PURPLE_CELL = pygame.image.load(os.path.join("assets/cells", "purple.png"))
 PINK_CELL = pygame.image.load(os.path.join("assets/cells", "pink.png"))
 RED_CELL = pygame.image.load(os.path.join("assets/cells", "red.png"))
 YELLOW_CELL = pygame.image.load(os.path.join("assets/cells", "yellow.png"))
 BLUE_CELL = pygame.image.load(os.path.join("assets/cells", "blue.png"))
+
+# Colors
+LIGHT_GREY = (209, 209, 209)
+DARK_GREY = (43, 43, 43)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Fonts
+ARIAL = pygame.font.SysFont('arial', 45)
+CHATHURA_RG = pygame.freetype.Font(os.path.join("assets/fonts/chathura", "chathura-regular.ttf"))
--- src/screen.py
+++ src/screen.py
@@ -1,29 +1,30 @@
 import pygame
+import pygame.freetype
 from board import *
-from smallboard import *
+from nextblock import *
 from assets import *
 
 # CONSTANTS
-SCREEN_WIDTH = 400  # px
-SCREEN_HEIGHT = 500  # px
+SCREEN_WIDTH = 570  # px
+SCREEN_HEIGHT = 700  # px
 FPS = 60
 
-# Board UI
-BOARD_CELL = 20  # 20 px square
+BOARD_CELL = 30  # 20 px square
 BOARD_X = 50  # Number of px from left edge of the screen
 BOARD_Y = 50  # Number of px from top of the screen
 
-SMALLBOARD_X = 280  # Number of px from left edge of the screen
-SMALLBOARD_Y = 50  # Number of px from top of the screen
+NEXT_BLOCK_AREA_X = 400  # Number of px from left edge of the screen
+NEXT_BLOCK_TEXT_AREA_Y = 50
+NEXT_BLOCK_AREA_Y = NEXT_BLOCK_TEXT_AREA_Y + 50  # Number of px from top of the screen
 
 # INITIALIZE
+pygame.init()
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tetris")
 
 
 # FUNCTIONS
-def updateMainBoard(board):
-    # Update board
+def updateBoard(board):
     for row in range(BOARD_HEIGHT):
         for col in range(BOARD_WIDTH):
             if board[row][col] == 0:
@@ -31,9 +32,9 @@ def updateMainBoard(board):
             elif board[row][col] == 1:
                 SCREEN.blit(GREEN_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
             elif board[row][col] == 2:
-                SCREEN.blit(INDIGO_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
+                SCREEN.blit(BRONZE_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
             elif board[row][col] == 3:
-                SCREEN.blit(ORANGE_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
+                SCREEN.blit(PURPLE_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
             elif board[row][col] == 4:
                 SCREEN.blit(PINK_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
             elif board[row][col] == 5:
@@ -43,28 +44,29 @@ def updateMainBoard(board):
             elif board[row][col] == 7:
                 SCREEN.blit(BLUE_CELL, (BOARD_X + col * BOARD_CELL, BOARD_Y + row * BOARD_CELL))
 
-    pygame.display.update()
+
+def updateNextBlockArea(next_block_area):
+    for row in range(NEXT_BLOCK_AREA_HEIGHT):
+        for col in range(NEXT_BLOCK_AREA_WIDTH):
+            if next_block_area[row][col] == 0:
+                SCREEN.blit(EMPTY_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 1:
+                SCREEN.blit(GREEN_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 2:
+                SCREEN.blit(BRONZE_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 3:
+                SCREEN.blit(PURPLE_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 4:
+                SCREEN.blit(PINK_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 5:
+                SCREEN.blit(RED_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 6:
+                SCREEN.blit(YELLOW_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+            elif next_block_area[row][col] == 7:
+                SCREEN.blit(BLUE_CELL, (NEXT_BLOCK_AREA_X + col * BOARD_CELL, NEXT_BLOCK_AREA_Y + row * BOARD_CELL))
+
+    drawText("Next", WHITE, 100, CHATHURA_RG, NEXT_BLOCK_AREA_X, NEXT_BLOCK_TEXT_AREA_Y)
 
 
-def updateSmallBoard(small_board):
-    # Update board
-    for row in range(SMALLBOARD_HEIGHT):
-        for col in range(SMALLBOARD_WIDTH):
-            if small_board[row][col] == 0:
-                SCREEN.blit(EMPTY_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 1:
-                SCREEN.blit(GREEN_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 2:
-                SCREEN.blit(INDIGO_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 3:
-                SCREEN.blit(ORANGE_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 4:
-                SCREEN.blit(PINK_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 5:
-                SCREEN.blit(RED_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 6:
-                SCREEN.blit(YELLOW_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-            elif small_board[row][col] == 7:
-                SCREEN.blit(BLUE_CELL, (SMALLBOARD_X + col * BOARD_CELL, SMALLBOARD_Y + row * BOARD_CELL))
-
-    pygame.display.update()
+def drawText(text, color, size, font, x, y):
+    font.render_to(SCREEN, (x, y), text, color, size=size)
