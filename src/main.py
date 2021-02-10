07c6beeebcf6c10f066994921a1d8807d1f07755
adaresa <kasper.vaus0@gmail.com>
2021-02-10 02:32:50 +0200

    Menu update
    Added menu functionality to main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from board import *
 from smallboard import *
 from blocks import *
 from screen import *
+from assets import *
 
 
 # MAIN
-def main():
-    pygame.init()
-    CLOCK = pygame.time.Clock()
+pygame.init()
+CLOCK = pygame.time.Clock()
 
+def game():
+    SCREEN.fill(BLACK)
     board = createBoard()  # 2D array, where "0" represents empty cell
     small_board = createSmallBoard()
 
@@ -26,7 +28,6 @@ def main():
     # Block automatic falling
     fall_timer = 0
     FALL_SPEED = 25  # Lower value -> Faster drop speed
-    updateSmallBoard(small_board)
 
     run = True
     while run:
@@ -46,12 +47,14 @@ def main():
         for event in pygame.event.get():
             # Close game
             if event.type == pygame.QUIT:
-                run = False
                 pygame.quit()
+                exit()
 
             # Move block
             if event.type == pygame.KEYDOWN:  # If a key is pressed down
                 key_timer = 0
+                if event.key == pygame.K_ESCAPE:
+                    run = False
                 if event.key == pygame.K_UP:
                     current_block.rotate(board)
                 elif event.key == pygame.K_DOWN:
@@ -84,10 +87,67 @@ def main():
             NextBlocks(next_block, small_board, 0, 1)
             third_block = randomBlock(small_board, 0, 6)
 
-            updateSmallBoard(small_board)
 
         # Screen
+        updateSmallBoard(small_board)
         updateMainBoard(board)
 
 
-main()
+def options():
+    run = True
+    while run:
+        SCREEN.fill((BLACK))
+        
+        draw_text('OPTIONS', font1, WHITE, SCREEN, 210, 30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+        
+        pygame.display.update()
+        CLOCK.tick(FPS)
+
+
+def main_menu():
+    while True:
+        SCREEN.fill((0,0,0))
+
+        mouse_x, mouse_y = pygame.mouse.get_pos() # Get mouse location
+        click = False
+ 
+        button_1 = pygame.Rect(150, 200, 200, 50)
+        button_2 = pygame.Rect(150, 300, 200, 50)
+
+        pygame.draw.rect(SCREEN, RED, button_1)
+        pygame.draw.rect(SCREEN, RED, button_2)
+        draw_text('TETRIS', font2, (100, 255, 255), SCREEN, 190, 45)
+        draw_text('MAIN MENU', font1, WHITE, SCREEN, 200, 115)
+        draw_text('NEW GAME', font1, WHITE, SCREEN, 205, 215)
+        draw_text('OPTIONS', font1, WHITE, SCREEN, 205, 315)
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+        
+        if button_1.collidepoint((mouse_x, mouse_y)):
+            if click:
+                game()
+        if button_2.collidepoint((mouse_x, mouse_y)):
+            if click:
+                options()
+ 
+        pygame.display.update()
+        CLOCK.tick(FPS)
+
+main_menu()
