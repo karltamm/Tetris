85928ee75acf7f27bd4738d42adae96ee1494c61
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-02-12 18:12:04 +0200

    Sound effects and game over check

--- src/main.py
+++ src/main.py
@@ -4,20 +4,23 @@ from nextblock import *
 from blocks import *
 from screen import *
 
-# Important initializing
+# Initialize pygame
 pygame.init()
 CLOCK = pygame.time.Clock()
 
-# IN GAME
-def game():
-    # Is game running (unpaused)?
-    game_running = True
+
+# Playing the game
+def startNewGame():
+    # Game states
+    game_running = True  # unpaused or not
+    game_over = False
 
     # Create button click areas
     pause_button = buttonClickBox(PAUSE_BTN_X, PAUSE_BTN_Y)
-    end_button = buttonClickBox(END_BTN_X, END_BTN_Y) # End game
+    end_button = buttonClickBox(END_BTN_X, END_BTN_Y)  # End game
+    new_game_button = buttonClickBox(NEW_GAME_BTN_X, NEW_GAME_BTN_Y)  # If game is over, this button will be shown
 
-    # Initialize first game
+    # Initialize game
     board = createBoard()
     next_block_area = createNextBlockArea()
     current_block = generateActiveBlock(board)
@@ -39,12 +42,15 @@ def game():
         # Update screen
         CLOCK.tick(FPS)
         SCREEN.fill(DARK_GREY)
+
         updateBoard(board)
         updateNextBlockArea(next_block_area)
         updateScore(score)
         updateGameButtons()
 
-        if not game_running:
+        if game_over:
+            updateGameOverScreen()
+        elif not game_running:
             updatePauseMenu()
 
         pygame.display.update()
@@ -59,15 +65,28 @@ def game():
                 run = False
                 pygame.quit()
 
+            # Check if game is over
+            if event.type == GAME_OVER:
+                game_running = False
+                game_over = True
+
             # Pause or unpause game
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+                if event.key == pygame.K_ESCAPE and not game_over:
                     game_running = not game_running
 
             # Buttons clicks
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    if pause_button.collidepoint(mouse_pos):
+                    if new_game_button.collidepoint(mouse_pos) and game_over:
+                        # Run following code only if game is over
+                        # new_game_button is shown when game is over
+                        # new_game_button is at the same place, where pause/unpause button (click areas overlap)
+
+                        # Start new game
+                        run = False  # End current game process!
+                        startNewGame()
+                    elif pause_button.collidepoint(mouse_pos):
                         # Pause or unpause game
                         game_running = not game_running
                     elif end_button.collidepoint(mouse_pos):
@@ -95,6 +114,8 @@ def game():
                     key_timer = 0
                     if event.key == pygame.K_UP:
                         current_block.rotate(board)
+                        # Sound effect for rotating
+                        ROTATE_SOUND.play()
                     elif event.key == pygame.K_DOWN:
                         down_pressed = True
                     elif event.key == pygame.K_RIGHT:
@@ -118,6 +139,10 @@ def game():
             if left_pressed and key_timer % 10 == 0:
                 current_block.move(board, -1, 0)
 
+            # Sound effect if moving a block
+            if (right_pressed or left_pressed or down_pressed) and key_timer % 10 == 0:
+                MOVE_SOUND.play()
+
             # Is current block placed?
             if current_block.is_placed:
                 full_rows = clearFullRows(board)
@@ -128,11 +153,16 @@ def game():
                 if full_rows > 0:
                     score += 100 + (full_rows - 1) * 200
 
-# MENU WINDOW
+                    # Sound effect if at least one row is cleared
+                    ROW_CLEARED_SOUND.play()
+
+
+# Main menu/splash screen
 def main_menu():
     # Create button click areas
     start_button = buttonClickBox(START_BTN_X, START_BTN_Y)  # New game
     quit_button = buttonClickBox(QUIT_BTN_X, QUIT_BTN_Y)  # Quit program
+
     run = True
     while run:
         # Update screen
@@ -140,7 +170,7 @@ def main_menu():
         SCREEN.fill(DARK_GREY)
         updateMainMenu()
         pygame.display.update()
-        
+
         # UI control
         mouse_pos = pygame.mouse.get_pos()
         events = pygame.event.get()
@@ -155,10 +185,12 @@ def main_menu():
                 if event.button == 1:
                     if start_button.collidepoint(mouse_pos):
                         # Start new game
-                        game()
+                        startNewGame()
                     elif quit_button.collidepoint(mouse_pos):
                         # Quit program
                         run = False
                         pygame.quit()
-    
+
+
+# Launch main menu if program is opened
 main_menu()
