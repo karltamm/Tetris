3a9cf41c55907f9d2029fb475eeba67a5c625c21
Karl-Heinrich Tamm <karl.heinrich@zoho.com>
2021-02-18 22:20:58 +0200

    Powers 0.1

--- src/screen.py
+++ src/screen.py
@@ -13,17 +13,21 @@ FPS = 60
 
 # Whitespace
 PADDING = 50
-NEAR = 10
+NEAR = 15
 FAR = 30
 
 # Text
-TILE_SIZE = 150
+TITLE_SIZE = 120
 HEADING1_SIZE = 100
+HEADING2_SIZE = 80
 TEXT_SIZE = 50
 
-TILE_HEIGHT = 100
-HEADING1_HEIGHT = 50
-TEXT_HEIGHT = 25
+HEIGHT_SIZE_RATIO = 0.417
+
+TITLE_HEIGHT = round(TITLE_SIZE * HEIGHT_SIZE_RATIO)
+HEADING1_HEIGHT = round(HEADING1_SIZE * HEIGHT_SIZE_RATIO)
+HEADING2_HEIGHT = round(HEADING2_SIZE * HEIGHT_SIZE_RATIO)
+TEXT_HEIGHT = round(TEXT_SIZE * HEIGHT_SIZE_RATIO)
 
 TITLE_FONT = CHATHURA_XBOLD
 HEADING_FONT = CHATHURA_RG
@@ -81,12 +85,17 @@ STAGE_VAL_Y = STAGE_TEXT_Y
 
 # Board
 BOARD_CELL = 30  # 30 px square
+BOARD_SCREEN_WIDTH = BOARD_WIDTH * BOARD_CELL
+BOARD_SCREEN_HEIGHT = BOARD_HEIGHT * BOARD_CELL
 
 BOARD_X = PADDING
 BOARD_Y = HIGH_SCORE_VAL_Y + TEXT_HEIGHT + FAR
 
+BOARD_X_END = BOARD_X + BOARD_SCREEN_WIDTH
+BOARD_Y_END = BOARD_Y + BOARD_SCREEN_HEIGHT
+
 # Next block area
-NEXT_BLOCK_TEXT_X = BOARD_X + BOARD_WIDTH * BOARD_CELL + PADDING
+NEXT_BLOCK_TEXT_X = BOARD_X_END + PADDING
 NEXT_BLOCK_TEXT_Y = BOARD_Y - (HEADING1_HEIGHT + NEAR)
 
 NEXT_BLOCK_AREA_X = NEXT_BLOCK_TEXT_X
@@ -102,6 +111,22 @@ RESUME_BTN_Y = PAUSE_BTN_Y
 END_BTN_X = PAUSE_BTN_X
 END_BTN_Y = PAUSE_BTN_Y + BTN_HEIGHT + NEAR
 
+# Powers
+POWERS_HEADING_X = NEXT_BLOCK_TEXT_X
+POWERS_HEADING_Y = NEXT_BLOCK_AREA_Y + NEXT_BLOCK_AREA_HEIGHT * BOARD_CELL + FAR
+
+# POWER_NAME_X = POWERS_HEADING_X
+# POWER_NAME_Y = POWERS_HEADING_Y + HEADING1_HEIGHT
+
+ACTIVATE_POWER_BTN_X = POWERS_HEADING_X
+ACTIVATE_POWER_BTN_Y = POWERS_HEADING_Y + HEADING1_HEIGHT + NEAR
+
+CLOSE_POWER_BTN_X = END_BTN_X
+CLOSE_POWER_BTN_Y = END_BTN_Y
+
+POWER_HELP_TXT_X = PADDING
+POWER_HELP_TXT_Y = BOARD_Y - TEXT_HEIGHT - NEAR
+
 # Game over screen
 GAME_OVER_TEXT_X = (SCREEN_WIDTH - 350) / 2
 GAME_OVER_TEXT_Y = (SCREEN_HEIGHT - HEADING1_HEIGHT) / 2
@@ -114,8 +139,7 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Tetris")
 
 
-# FUNCTIONS
-# General
+# GENERAL FUNCTIONS
 def drawText(text, x, y, size=TEXT_SIZE, color=WHITE, font=TEXT_FONT):
     font.render_to(SCREEN, (x, y), text, color, size=size)
 
@@ -154,7 +178,7 @@ def checkButtonCorner(mouse_x, mouse_y, button_x, button_y):  # Checks if mouse 
         return True
 
 
-# Game UI
+# GAME
 def updateBoard(board):
     for row in range(BOARD_HEIGHT):
         for col in range(BOARD_WIDTH):
@@ -220,10 +244,22 @@ def updateGameButtons():
     drawButton(END_BTN, END_BTN_X, END_BTN_Y)
 
 
+def updatePowersSelection(power):
+    drawText("Power", POWERS_HEADING_X, POWERS_HEADING_Y, size=HEADING1_SIZE, font=HEADING_FONT)
+
+    if power.is_available:
+        if power.name == "Laser":
+            button = LASER_BTN
+
+        drawButton(button, ACTIVATE_POWER_BTN_X, ACTIVATE_POWER_BTN_Y)
+    else:
+        drawText("Not available", POWER_NAME_X, POWER_NAME_Y, size=TEXT_SIZE, font=HEADING_FONT, color=LIGHT_GREY)
+
+
 def updatePauseMenu():
     # Background
     transparent_bg = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
-    transparent_bg.fill((0, 0, 0, 200))  # Last number represents opacitiy [0 = no opacity]
+    transparent_bg.fill(TRANSPARENT_BLACK)  # Last number represents opacitiy [0 = no opacity]
     SCREEN.blit(transparent_bg, (0, 0))
 
     # Buttons
@@ -234,17 +270,18 @@ def updatePauseMenu():
 def updateGameOverScreen():
     # Background
     transparent_bg = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
-    transparent_bg.fill((0, 0, 0, 200))  # Last number represents opacitiy [0 = no opacity]
+    transparent_bg.fill(TRANSPARENT_BLACK)  # Last number represents opacitiy [0 = no opacity]
     SCREEN.blit(transparent_bg, (0, 0))
 
     # Message
-    drawText("Game Over", GAME_OVER_TEXT_X, GAME_OVER_TEXT_Y, size=TILE_SIZE, font=TITLE_FONT)
+    drawText("Game Over", GAME_OVER_TEXT_X, GAME_OVER_TEXT_Y, size=TITLE_SIZE, font=TITLE_FONT)
 
     # Buttons
     drawButton(NEW_GAME_BTN, NEW_GAME_BTN_X, NEW_GAME_BTN_Y)
     drawButton(END_BTN, END_BTN_X, END_BTN_Y)
 
 
+# MAIN MENU
 def updateMainMenu():
     SCREEN.blit(LOGO, (LOGO_X, LOGO_Y))
 
@@ -255,3 +292,42 @@ def updateMainMenu():
     drawButton(QUIT_BTN, QUIT_BTN_X, QUIT_BTN_Y)
 
     SCREEN.blit(INSTRUCTION_IMAGE, (INSTRUCTION_X, INSTRUCTION_Y))
+
+
+# POWERS
+# Laser
+def laserScreen(row):
+    highlightBoard()
+    highlightRow(*row)
+    showPowerHelpText("Click on a row to remove it")
+
+
+def highlightBoard():
+    # Create pieces of screen covers
+    top_side = pygame.Surface((SCREEN_WIDTH, BOARD_Y), pygame.SRCALPHA)
+    left_side = pygame.Surface((BOARD_X, SCREEN_HEIGHT), pygame.SRCALPHA)
+    right_side = pygame.Surface((SCREEN_WIDTH - BOARD_X_END, SCREEN_HEIGHT), pygame.SRCALPHA)
+    bottom_side = pygame.Surface((BOARD_SCREEN_WIDTH, SCREEN_HEIGHT - BOARD_Y_END), pygame.SRCALPHA)
+
+    # Make every piece transparent
+    top_side.fill(DARK_GREY)
+    left_side.fill(DARK_GREY)
+    right_side.fill(DARK_GREY)
+    bottom_side.fill(DARK_GREY)
+
+    # Place pieces on right positions
+    SCREEN.blit(top_side, (0, 0))
+    SCREEN.blit(left_side, (0, BOARD_Y))
+    SCREEN.blit(right_side, (BOARD_X_END, BOARD_Y))
+    SCREEN.blit(bottom_side, (BOARD_X, BOARD_Y_END))
+
+
+def showPowerHelpText(text):
+    drawText(text, POWER_HELP_TXT_X, POWER_HELP_TXT_Y)
+
+
+def highlightRow(row_y, row_index=0):
+    if row_y is not None:
+        highlight = pygame.Surface((BOARD_SCREEN_WIDTH, BOARD_CELL), pygame.SRCALPHA)
+        highlight.fill(TRANSPARENT_WHITE)
+        SCREEN.blit(highlight, (BOARD_X, row_y))
